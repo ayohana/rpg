17eef1c5464af572fbb74d838593e446808c6d9b
Adela Darmansyah and Erik Grove <adela@>
2020-02-05 09:30:18 -0800

    Add classes for characters

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 // import $ from 'jquery';
 import { thingFunction } from './rpg.js'
-import { char } from './rpg'
+import { officeWorker } from './rpg'
 
 console.log(thingFunction);
+
+var testCharacter = new officeWorker();
+console.log(testCharacter);
+console.log(testCharacter.name);
--- src/rpg.js
+++ src/rpg.js
@@ -2,10 +2,31 @@ export function thingFunction(){
   return "I do a thing coming from thing.js";
 }
 
-export const char = {
-  officeWorker: "Anon",
-  coworker: "Jeff",
-  daughter: "Ellie",
+export class officeWorker {
+  constructor() {
+    this.name = "Anon";
+    this.IQ = 129;
+    this.mana = 100;
+    this.HP = 100;
+  }
 }
 
-export const { officeWorker, coworker, daughter } = char;
+export class coworker {
+  constructor() {
+    this.name = "Jeff";
+    this.IQ = 115;
+    this.mana = 100;
+    this.HP = 80;
+  }
+}
+
+export class daughter {
+  constructor() {
+    this.name = "Ellie";
+    this.IQ = 110;
+    this.mana = 150;
+    this.HP = 50;
+  }
+}
+
+// export const { name, IQ, mana, HP } = officeWorker;
