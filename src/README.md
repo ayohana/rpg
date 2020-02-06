440052fb454593bcaf5a09e9e71c3f377e7399b1
Adela Darmansyah and Erik Grove <erik@>
2020-02-05 16:24:08 -0800

    Update README.md

---
+++ src/README.md
@@ -0,0 +1,37 @@
+# Seattle Ain't What it Cracked Up To Be
+
+#### Javascript and Jest exercise for Epicodus, 02/05/2020
+
+#### By Erik Grove and Adela Darmansyah
+
+## Description
+
+An adventure game set in Seattle where you rescue your daughter from the world-famous McStabby's on 3rd and Pine.
+
+## Setup/Installation Requirements
+
+* Open `index.html` in your preferred browser.
+* _Have fun!_
+
+## Known Bugs
+
+No known bugs at this time.
+
+## Support and contact details
+
+Email no one for feedback/questions.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Javascript
+* Jest
+* npm
+* Webpack
+
+### License
+
+*This web application is licensed under the MIT license.*
+
+Copyright (c) 2020 **Erik Grove and Adela Darmansyah**
--- src/main.js
+++ src/main.js
@@ -5,15 +5,31 @@ import $ from 'jquery';
 import { Player, Game } from '../src/rpg';
 
 function showCorrectStory(game) {
-  if (game.currentSpace.xCoordinate === 3 && game.currentSpace.yCoordinate === 2) {
-    $("#space3-2").show();
-    $("#space1-2").hide();
-  } else if (game.currentSpace.xCoordinate === 1 && game.currentSpace.yCoordinate === 2) {
+  if (game.currentSpace.xCoordinate === 1 && game.currentSpace.yCoordinate === 2) {
     $("#space1-2").show();
+    $("#space2-1").hide();
+    $("#space2-3").hide();
     $("#space3-2").hide();
-  } else {
-    $("#space3-2").hide();
+  } else if (game.currentSpace.xCoordinate === 2 && game.currentSpace.yCoordinate === 1) {
     $("#space1-2").hide();
+    $("#space2-1").show();
+    $("#space2-3").hide();
+    $("#space3-2").hide();
+  } else if (game.currentSpace.xCoordinate === 2 && game.currentSpace.yCoordinate === 3) {
+    $("#space1-2").hide();
+    $("#space2-1").hide();
+    $("#space2-3").show();
+    $("#space3-2").hide();
+  } else if (game.currentSpace.xCoordinate === 3 && game.currentSpace.yCoordinate === 2) {
+    $("#space3-2").show();
+    $("#space2-1").hide();
+    $("#space2-3").hide();
+    $("#space1-2").hide();
+  } else {
+    $("#space1-2").hide();
+    $("#space2-1").hide();
+    $("#space2-3").hide();
+    $("#space3-2").hide();
   }
 }
 
